3721a7f1cf782df49c4ad63710002b21ce0f083e
boguslawblonski <bblonski@o2.pl>
2020-02-14 11:19:48 +0100

    add hero

--- helloflutterapp/lib/dashboard.dart
+++ helloflutterapp/lib/dashboard.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
+import 'detailPage.dart';
+
 class DashboardPage extends StatelessWidget {
   // Declare a field that holds the Todo.
   String todo;
@@ -15,62 +17,100 @@ class DashboardPage extends StatelessWidget {
       appBar: AppBar(
         title: Text(todo),
       ),
-      body: Column(
-        children: <Widget>[
-          SizedBox(height: 150),
-          Padding(
-            padding: EdgeInsets.all(30),
-            child:  Stack(
-              overflow: Overflow.visible,
-              alignment: AlignmentDirectional.center,
-              children: <Widget>[
-                Positioned(
-                  bottom: -50,
-                  right: -50,
-                  child: Container(
-                    height: 100,
-                    width: 100,
-                    color: Colors.blueAccent,
-                    child: SizedBox(),
-                  ),
-                ),
-                Opacity(
-                  opacity: 1,
-                  child: Container(
-                      width: 100,
+      body: Center(
+        child: Column(
+          children: <Widget>[
+            SizedBox(height: 150),
+            Padding(
+              padding: EdgeInsets.all(30),
+              child:  Stack(
+                overflow: Overflow.visible,
+                alignment: AlignmentDirectional.center,
+                children: <Widget>[
+                  Positioned(
+                    top: -50,
+                    left: -50,
+                    child: Container(
                       height: 100,
-                      color: Colors.green,
-                      child: SizedBox()
-                  ),
-                ),
-                Positioned(
-                  top: -50,
-                  left: -50,
-                  child: Hero(
-                    tag: "me",
-                    child: Image.asset('assets/images/me.jpg' , height: 100),
-                  ),
-                ),
-                Positioned(
-                  bottom: 200,
-                  child: RichText(
-                    text: TextSpan(
-                        style: TextStyle(color: Colors.cyan, fontSize: 30, fontWeight: FontWeight.bold),
-                        children: <TextSpan>[
-                          TextSpan(text: "Welcome "),
-                          TextSpan(text: todo , style: TextStyle(
-                              decoration: TextDecoration.underline,
-                              color: Colors.blueAccent,
-                              fontStyle: FontStyle.italic
-                          ))
-                        ]
+                      width: 100,
+                      color: Colors.blueAccent,
+                      child: SizedBox(),
                     ),
                   ),
-                )
-              ],
+                  Positioned(
+                    bottom: -50,
+                    right: -50,
+                    child: Opacity(
+                      opacity: 1,
+                      child: Container(
+                          width: 100,
+                          height: 100,
+                          color: Colors.green,
+                          child: SizedBox()
+                      ),
+                    ),
+                  ),
+                  Positioned(
+                    child: Hero(
+                      tag: "me",
+                      child: Image.asset('assets/images/me.jpg', height: 100),
+                    ),
+                  ),
+                  Positioned(
+                    bottom: 200,
+                    child: RichText(
+                      text: TextSpan(
+                          style: TextStyle(color: Colors.cyan, fontSize: 30, fontWeight: FontWeight.bold),
+                          children: <TextSpan>[
+                            TextSpan(text: "Welcome "),
+                            TextSpan(text: todo , style: TextStyle(
+                                decoration: TextDecoration.underline,
+                                color: Colors.blueAccent,
+                                fontStyle: FontStyle.italic
+                            ))
+                          ]
+                      ),
+                    ),
+                  )
+                ],
+              ),
             ),
-          ),
-        ],
+            SizedBox(height: 40),
+            RichText(
+                text: TextSpan(
+                    style: TextStyle(fontStyle: FontStyle.normal,color: Colors.black, fontSize: 23),
+                    children: <TextSpan>[
+                      TextSpan(text: '.Net developer', style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 30,
+                          color: Colors.purpleAccent
+                      )),
+                      TextSpan(text: " in ", style: TextStyle(
+                      )),
+                      TextSpan(text: 'Flutter', style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 30,
+                          color: Colors.purpleAccent
+                      ))
+                    ]
+                )
+            ),
+            RaisedButton(
+              textColor: Colors.white,
+              color: Colors.blue,
+              child: Text('Do I think cross-platform ?'),
+              onPressed: (){
+
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => DetailPage(todo: todo)),
+                );
+
+              },
+            ),
+
+          ],
+        ),
       ),
 
     );
--- helloflutterapp/lib/detailPage.dart
+++ helloflutterapp/lib/detailPage.dart
@@ -17,80 +17,16 @@ class DetailPage extends StatelessWidget {
       ),
       body: Column(
         children: <Widget>[
-          Center(
-            child: Padding(
-              padding: EdgeInsets.all(16.0),
-              child:  Stack(
-                overflow: Overflow.visible,
-                alignment: AlignmentDirectional.center,
-                children: <Widget>[
-                  Positioned(
-                    bottom: -50,
-                    right: -50,
-                    child: Container(
-                      height: 100,
-                      width: 100,
-                      color: Colors.deepOrangeAccent,
-                      child: SizedBox(),
-                    ),
-                  ),
-                  Opacity(
-                    opacity: 1,
-                    child: Container(
-                        width: 100,
-                        height: 100,
-                        color: Colors.green,
-                        child: SizedBox()
-                    ),
-                  ),
-                  Positioned(
-                    top: -50,
-                    left: -50,
-                    child: Container(
-                      height: 100,
-                      width: 100,
-                      //color: Colors.blueAccent,
-                      child: Image(image: AssetImage('assets/images/me.jpg')),
-                    ),
-                  ),
-                  Positioned(
-                    bottom: 200,
-                    child: RichText(
-                      text: TextSpan(
-                          style: TextStyle(color: Colors.cyan, fontSize: 30, fontWeight: FontWeight.bold),
-                          children: <TextSpan>[
-                            TextSpan(text: "Welcome "),
-                            TextSpan(text: todo , style: TextStyle(
-                                decoration: TextDecoration.underline,
-                                color: Colors.blueAccent,
-                                fontStyle: FontStyle.italic
-                            ))
-                          ]
-                      ),
-                    ),
-                  )
-                ],
-              ),
-            ),
+          Hero(
+            tag: "me",
+            child: Image.asset('assets/images/me.jpg'),
           ),
-          RaisedButton(
-            textColor: Colors.white,
-            color: Colors.blue,
-            child: Text('Login'),
-            onPressed: (){
-
-
-            },
-          ),
-          Image(image: AssetImage('assets/images/me.jpg')),
-          Image.asset('assets/images/me.jpg'),
           Text(
-            "Go to GestureOnRowRoute",
+            "Trying being open-minded ?",
             style: TextStyle(fontSize: 20.0),
           ),
         ],
       ),
-
     );
   }
 }
