9f8eb3d8eaf06b319717287d7b77528e9309b3cc
boguslawblonski <bblonski@o2.pl>
2020-02-14 11:38:56 +0100

    chart

--- helloflutterapp/lib/detailPage.dart
+++ helloflutterapp/lib/detailPage.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:charts_flutter/flutter.dart' as charts;
 
 class DetailPage extends StatelessWidget {
   // Declare a field that holds the Todo.
@@ -10,7 +11,41 @@ class DetailPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // Use the Todo to create the UI.
+
+    var data = [
+      ClicksPerYear('Flash', 15, Colors.red),
+      ClicksPerYear('JAVA', 50, Colors.deepOrange),
+      ClicksPerYear('.NET', 90, Colors.blue),
+      ClicksPerYear('ASP', 60, Colors.blue),
+      ClicksPerYear('Desctop', 60, Colors.blue),
+      ClicksPerYear('Xamarin ', 70, Colors.blue),
+      ClicksPerYear('Flutter', 10, Colors.purple)
+    ];
+
+    var series = [
+      charts.Series(
+        domainFn: (ClicksPerYear clickData, _) => clickData.year,
+        measureFn: (ClicksPerYear clickData, _) => clickData.clicks,
+        colorFn: (ClicksPerYear clickData, _) => clickData.color,
+        id: 'Clicks',
+        data: data,
+      ),
+    ];
+
+    var chart = charts.BarChart(
+      series,
+      animate: true,
+    );
+
+    var chartWidget = Padding(
+      padding: EdgeInsets.all(32.0),
+      child: SizedBox(
+        height: 200.0,
+        child: chart,
+      ),
+    );
+
+
     return Scaffold(
       appBar: AppBar(
         title: Text(todo),
@@ -22,11 +57,22 @@ class DetailPage extends StatelessWidget {
             child: Image.asset('assets/images/me.jpg'),
           ),
           Text(
-            "Trying being open-minded ?",
+            "Trying being open-minded:",
             style: TextStyle(fontSize: 20.0),
           ),
+          chartWidget
         ],
       ),
     );
   }
+}
+
+class ClicksPerYear {
+  final String year;
+  final int clicks;
+  final charts.Color color;
+
+  ClicksPerYear(this.year, this.clicks, Color color)
+      : this.color = charts.Color(
+      r: color.red, g: color.green, b: color.blue, a: color.alpha);
 }
